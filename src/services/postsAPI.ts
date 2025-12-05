c268f6850accb876574fc1d624032a7914f9571c
Назар Івченко <nazary9911@gmail.com>
2025-12-05 05:34:25 +0200

    feature/post/update

--- src/services/postsAPI.ts
+++ src/services/postsAPI.ts
@@ -21,10 +21,11 @@ export const getCategories = async () => {
 };
 
 export const getPopularPosts = async () => {
-  const popularPosts = await apiData.get(
-    '/post2s?populate[category_2][populate]=*&populate[author_2][populate]=*&populate[image][populate]=*&filters[isPopular][$eq]=true&pagination[page]=1&pagination[pageSize]=3',
+  const res = await apiData.get(
+    '/post2s?populate[category_2][populate]=*&populate[author_2][populate]=*&populate[image][populate]=*&sort=createdAt:desc&pagination[page]=1&pagination[pageSize]=5',
   );
-  return popularPosts.data;
+
+  return res.data; // возвращаем объект целиком
 };
 
 export const getPost = async documentId => {
